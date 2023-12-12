61f044dcd6aeefe60e9626a5034bca7d9534d429
Charles Tay <charles.tay@lumiq.com>
2023-12-12 13:03:23 +0800

    feat: update template

--- src/day0/index.ts
+++ src/day0/index.ts
@@ -1,18 +1,23 @@
-import { Day } from "../day";
+import { Day } from '../day';
 
 class Day0 extends Day {
+  constructor() {
+    super(0);
+  }
 
-    constructor(){
-        super(0);
-    }
+  parseInput(input: string): string[] {
+    return input.split('\n');
+  }
 
-    solveForPartOne(input: string): string {
-        return input;
-    }
+  solveForPartOne(input: string): string {
+    const lines = this.parseInput(input);
+    return '';
+  }
 
-    solveForPartTwo(input: string): string {
-        return input;
-    }
+  solveForPartTwo(input: string): string {
+    const lines = this.parseInput(input);
+    return '';
+  }
 }
 
-export default new Day0;
+export default new Day0();
